8743fd77305b8e982ced3921def512b7f6f23e36
RoMalms10 <156@holbertonschool.com>
2018-03-07 21:26:17 +0000

    Fix betty compliances and add Return description

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,19 +5,20 @@
  * binary_tree_node - Creates a node in a binary tree
  * @parent: Parent node to point parent pointer to
  * @value: Value to point node->n
+ * Return: Pointer to the new node, or NULL
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-        new_node = malloc(sizeof(binary_tree_t));
-        if (new_node == NULL)
-                return NULL;
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-        new_node->parent = parent;
-        new_node->n = value;
-        new_node->left = NULL;
-        new_node->right = NULL;
-        return (new_node);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
