d1081d3e17ca3671c811ab67ec6cb1f51b59b4a0
Dimitrios Philliou <DimitriCat@github.com>
2018-03-10 02:02:19 +0000

    Checking if a binary tree is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -3,61 +3,62 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Return the height of node in a binary tree
- * @tree: Pointer to node
+ * binary_tree_size - Return the size of a binary tree
+ * @tree: Pointer to root node
  * Return: 0 on failure and 1 on success
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t l_height, r_height;
-
 	if (tree == NULL)
 		return (0);
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	l_height = binary_tree_height(tree->left) + 1;
-	r_height = binary_tree_height(tree->right) + 1;
-	if (l_height >= r_height)
-		return (l_height);
-	else
-		return (r_height);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
 
+
 /**
- * binary_tree_balance - Return the difference between the left and right sides
- * @tree: Pointer to root node
- * Return: Number of balance with a child
+ * b_helper - Indexes all the nodes ina a binary tree
+ * @tree: Pointer to node
+ * @index: First index of binary tree
+ * @size: Number of nodes in the binary tree
+ * Return: 0 on failure and 1 on success
  */
 
-int binary_tree_balance(const binary_tree_t *tree)
+int b_helper(const binary_tree_t *tree, size_t index, size_t size)
 {
 	int left, right;
 
 	if (tree == NULL)
+		return (1);	
+
+	if (index > size)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-	return (left - right);
+	left = b_helper(tree->left, (index * 2) + 1, size);
+	right = b_helper(tree->right, (index * 2) + 2, size);
+
+	if (left == 0 || right == 0)
+		return (0);
+	return (1);
 }
 
 /**
- * binary_tree_is_complete - Checks if a binary tree is complete
- * @tree: Pointer to root node
- * Return: 1 if complete, 0 if not complete
+ * binary_tree_is_complete - See if a binary tree is complete 
+ * @tree: Pointer to node
+ * Return: 1 if complete, 0 if not
  */
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+	int result;
+
 	if (tree == NULL)
 		return (0);
-	if (tree->right && tree->left == NULL)
-		return (0);
-	if (binary_tree_balance(tree) < 0)
-		return (0);
-	left = binary_tree_is_complete(tree->left);
-	binary_tree_is_complete(tree->right);
-	return (1);
+
+	result = b_helper(tree, 0, binary_tree_size(tree) - 1);
+	return (result);	
 }
